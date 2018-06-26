baf51c9d64a9fc2e8eb68d764a68a73d5e32c1dc
Bolivar <diegofd@gmail.co>
2018-06-25 21:28:07 -0500

    PrimerCommit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Aquaman' },
+  { id: 14, name: 'Tormenta' },
+  { id: 15, name: 'Wolverine' },
+  { id: 16, name: 'Hulk' },
+  { id: 17, name: 'Hercules' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
